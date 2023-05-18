056b2216c0452cacfc0385dc3b95d962f01c4a77
Blend <blendhasani500@gmail.com>
2023-05-18 21:08:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// this code gives functionality of the accordion components.
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const faq = button.nextElementSibling;
+    const icon = button.children[1];
+
+    faq.classList.toggle("show");
+    icon.classList.toggle("rotate");
+  });
+});
